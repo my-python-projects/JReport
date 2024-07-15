30a89e28fbe0fb07935a57cffcdb497e963845e1
Jefferson Gomes <jeffsilvaygo@gmail.com>
2024-07-15 18:03:26 -0300

    add MONGO_URI in conf tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,10 +2,26 @@ import sys
 import os
 import pytest
 
+
 # Adiciona o diretório 'backend' ao caminho de importação do Python
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from log_config import setup_test_logging
+from config import Config
+from app import create_app
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+        "MONGO_URI": Config.MONGO_URI
+    })
+    return app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
 
 @pytest.fixture(autouse=True)
 def log_tests(request):
